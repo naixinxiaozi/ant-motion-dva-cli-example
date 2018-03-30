e7dac7afe83a263203dc57ac9e8c13203448b899
andy <zhangke002@foxmail.com>
2018-03-30 10:57:27 +0800

    update

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import enquire from 'enquire.js';
-import ScrollAnim from 'rc-scroll-anim';
+import { enquire } from 'enquire-js';
 
+import Content5 from './Content5';
 import Nav from './Nav';
-import Content0 from './Content0';
+import Content6 from './Content6';
 import Content1 from './Content1';
+import Content0 from './Content0';
 import Content2 from './Content2';
 import Content3 from './Content3';
+import Content4 from './Content4';
+import Content7 from './Content7';
+import Content8 from './Content8';
+import Content9 from './Content9';
+import Content10 from './Content10';
 import Footer from './Footer';
-import Point from './Point';
 
 import './less/antMotion_style.less';
 
-const scrollScreen = ScrollAnim.scrollScreen;
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -29,6 +32,7 @@ export default class Home extends React.Component {
     this.enquireScreen((isMode) => {
       this.setState({ isMode });
     });
+
     // dva 2.0 样式在组件渲染之后动态加载，导致滚动组件不生效；线上不影响；
     if (location.port) {
       // 样式 build 时间在 200-300ms 之间;
@@ -51,18 +55,23 @@ export default class Home extends React.Component {
       },
     });
     /* eslint-enable no-unused-expressions */
-  }
+  };
 
   render() {
     const children = [
-      <Nav id="nav_0_0" key="nav_0_0" isMode={this.state.isMode} />,
-      <Content0 id="content_10_0" key="content_10_0" isMode={this.state.isMode} />,
+      <Nav id="nav_1_0" key="nav_1_0" isMode={this.state.isMode} />,
+      <Content0 id="content_0_0" key="content_0_0" isMode={this.state.isMode} />,
+      <Content5 id="content_10_0" key="content_10_0" isMode={this.state.isMode} />,
+      <Content6 id="content_9_0" key="content_9_0" isMode={this.state.isMode} />,
       <Content1 id="content_2_0" key="content_2_0" isMode={this.state.isMode} />,
       <Content2 id="content_3_0" key="content_3_0" isMode={this.state.isMode} />,
       <Content3 id="content_4_0" key="content_4_0" isMode={this.state.isMode} />,
-      <Footer id="footer_0_0" key="footer_0_0" isMode={this.state.isMode} />,
-       // 导航和页尾不进入锚点区，如果需要，自行添加;
-      <Point key="list" ref="list" data={['content_10_0', 'content_2_0', 'content_3_0', 'content_4_0']} />,
+      <Content4 id="content_1_0" key="content_1_0" isMode={this.state.isMode} />,
+      <Content7 id="content_5_0" key="content_5_0" isMode={this.state.isMode} />,
+      <Content8 id="content_6_0" key="content_6_0" isMode={this.state.isMode} />,
+      <Content9 id="content_7_0" key="content_7_0" isMode={this.state.isMode} />,
+      <Content10 id="content_8_0" key="content_8_0" isMode={this.state.isMode} />,
+      <Footer id="footer_1_0" key="footer_1_0" isMode={this.state.isMode} />,
     ];
     return (
       <div className="templates-wrapper">
